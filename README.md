01681c87bc592c56fc6498d6dbcdf345ba55b715
Alexander Sandberg <hi@alexandersandberg.com>
2019-07-07 23:08:29 +0200

    Add project

---
+++ README.md
@@ -0,0 +1,41 @@
+![Keanu Reeves](dist/preview.jpg)
+
+# PlaceKeanu.com
+For when you need a little more Keanu in your life
+
+## How to use this *lifechanging* service
+Simply paste a link in this format, wherever you need him: `https://placekeanu.com/[width]/[height]*/[options]*`<br>
+(* optional)
+
+If you don't specify a height, you'll get a **square** Keanu...<br>
+...size-wise, that is, because Keanu is always mindblowingly awesome.
+
+## Available [options]
+* Young Keanus only: `y`
+* Grayscale: `g`<br>
+Note: Some Keanus are grayscale by default.
+
+## Some examples
+* Any Keanu: [https://placekeanu.com/200/150](https://placekeanu.com/200/150)
+* Young Keanu: [https://placekeanu.com/500/300/y](https://placekeanu.com/500/300/y)
+* Square + Grayscale: [https://placekeanu.com/250/g](https://placekeanu.com/250/g)
+* Young + Grayscale: [https://placekeanu.com/300/550/yg](https://placekeanu.com/300/550/yg)
+
+## How does it work?
+When you send a valid request to this website, a random photo of [Keanu Reeves](https://en.wikipedia.org/wiki/Keanu_Reeves) is displayed.
+
+The photos are cached on your end for 1 day, which is why you don't get a new Keanu on every page load.
+
+While some placeholder image websites dynamically create new images, this service is SVG-based. For this to work properly, all photos have been encoded to Base64.
+
+## Contributing
+If you found a bug that needs fixing, or have a feature you want to discuss or implement, feel free to open an issue or a pull request. Any contribution is appreciated, big or small!
+
+## License
+All Keanus belong to their respective owners. See the full [Keanu list](https://placekeanu.com/keanus) for photo attributions.
+
+The service itself is open-source, and you can find the source code [on GitHub](https://github.com/alexandersandberg/placekeanu.com).
+
+---
+
+Thanks for checking out my project! _You're breathtaking_. ❤️
---
+++ src/server.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+
+const photos = require('../dist/photos.json');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/keanus', (req, res) => {
+  res.sendFile(path.join(__dirname, 'keanus.html'));
+});
+
+app.get('/error', (req, res) => {
+  res.sendFile(path.join(__dirname, 'error.html'));
+});
+
+app.get('/photos.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'photos.json'));
+});
+
+app.get('/equalize.min,css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'equalize.min.css'));
+});
+
+app.get('/:width/:height?/:options?', (req, res) => {
+  // assign parameters from request to a variable
+  const params = req.params;
+
+  // assign each parameter into a separate variable
+  const width = parseInt(params.width, 10);
+  let height = parseInt(params.height, 10);
+
+  // define ratio between requested width and height
+  const ratio = width/height;
+
+  // if no options are set, assign an empty string to the variable
+  let options = typeof params.options !== 'undefined' ? params.options : '';
+
+  if (typeof height === 'undefined') {
+    // if only width is set, set height equal to width to return a square image
+    height = width;
+  } else if (isNaN(height) === true) {
+    // if options are passed to the second parameter in the url, return a
+    // square image and assign the options to the correct variable
+    height = width;
+    options = params.height;
+  }
+
+  // if no width or height is set, redirect to home
+  if (isNaN(width) === true || isNaN(height) === true) {
+    res.redirect(302, './error');
+    res.end();
+  } else {
+    // set each variable to true if respective option is present in path
+    const grayscale = /[g]/.test(options);
+    const young = /[y]/.test(options);
+
+    // get photo from json data
+    const getPhoto = () => {
+      // choose a random photo
+      const chosenPhoto = photos[Math.floor(Math.random() * photos.length)];
+
+      // define ratio between chosen photo's width and height
+      const chosenPhotoRatio = chosenPhoto.width/chosenPhoto.height;
+
+      // try again if...
+      // * young option is set, but chosen photo is not
+      // * photo ratio is too extreme, and chosen photo is not correct rotation
+      // otherwise, return the chosen photo
+      if ((young === true && chosenPhoto.young !== true) || (ratio < 0.75 && chosenPhotoRatio > 1) || (ratio > 1.25 && chosenPhotoRatio < 1)) {
+        return getPhoto();
+      } else {
+        return chosenPhoto;
+      }
+    }
+    const photo = getPhoto();
+
+    // if grayscale option is set, add a filter inside the svg
+    // and a filter attribute on the image
+    let filter = '';
+    let filterAttribute = '';
+    if (grayscale === true) {
+      filter = '<filter id="filter"><feColorMatrix type="saturate" values="0.10"/></filter>';
+      filterAttribute = ' filter="url(#filter)"';
+    }
+
+    // set custom headers to tell the client we're sending an svg, and enable
+    // caching of the image
+    res.writeHead(200, {'Content-Type': 'image/svg+xml', 'Cache-Control': 'public, max-age=86400'});
+
+    // end the response process by sending the requested svg
+    res.end('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + width + '" height="' + height + '" viewBox="0 0 ' + photo.width + ' ' + photo.height + '" preserveAspectRatio="xMidYMid slice">' + filter + '<image xlink:href="' + photo.data + '" width="' + photo.width + '" height="' + photo.height + '"' + filterAttribute + ' /></svg>');
+  }
+});
+
+app.use('/.netlify/functions/server', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
